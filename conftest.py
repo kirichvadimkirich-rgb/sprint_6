d0b08fd3b6d190a8a6ee6d987196ce2a04872e67
vadim <vadimm87@mail.ru>
2026-03-22 19:28:12 +0300

    убрать лишний ипорт

--- conftest.py
+++ conftest.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from locators.main_page_locators import ACCEPT_COOKIES
 from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
-from selenium.webdriver.support.ui import WebDriverWait
 
 BASE_URL = "https://qa-scooter.praktikum-services.ru/"
 LINK_YANDEX_REDIRECT = "https://dzen.ru/?yredirect=true"
