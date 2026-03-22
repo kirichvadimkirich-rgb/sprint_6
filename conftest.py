2adbbb23ed1ebf8218720e8c8c98df6f2159ee67
vadim <vadimm87@mail.ru>
2026-03-22 20:22:16 +0300

    исправить фикстуры

--- conftest.py
+++ conftest.py
@@ -3,23 +3,24 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from locators.main_page_locators import ACCEPT_COOKIES
 from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
-
-BASE_URL = "https://qa-scooter.praktikum-services.ru/"
-LINK_YANDEX_REDIRECT = "https://dzen.ru/?yredirect=true"
+from url import BASE_URL
 
 @pytest.fixture
 #Фикстура для открытия и закрытия драйвера Firefox,  принимаем куки, если есть
-def browser():
+def driver():
     firefox_options = FirefoxOptions()
     firefox_options.add_argument("--width=1920")
     firefox_options.add_argument("--height=1080")
     driver = webdriver.Firefox(options=firefox_options)
+    yield driver
+    driver.quit()
+
+@pytest.fixture
+def browser(driver):
     driver.get(BASE_URL)
     try:
         driver.find_element(*ACCEPT_COOKIES).click()
     except (NoSuchElementException, ElementClickInterceptedException):
-        pass
-
-    yield driver
-    driver.quit()
+        # Если кнопка не найдена или не кликабельна — просто игнорируем
+        pass   
 
